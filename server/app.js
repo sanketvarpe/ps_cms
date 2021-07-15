5ebed8d4207ff9edffd2549616106e888e639bbc
sanketvarpe <varpesanket2000@gmail.com>
2021-07-16 01:11:05 +0530

    modified gitignore

--- server/app.js
+++ server/app.js
@@ -65,7 +65,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(session({
     resave: true,
     saveUninitialized: true,
-    secret: '4dqoDpdnp@1u30938wufwe@1488',
+    secret: process.env.SESSION_SECRET,
     cookie: { maxAge: 1209600000 }, // two weeks in milliseconds
     store: MongoStore.create({
       mongoUrl: MONGODB_URI
